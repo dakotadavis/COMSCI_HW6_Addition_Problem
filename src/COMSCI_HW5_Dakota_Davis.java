7c4cb13881ddae19ee4f9b025ece8772596b48fb
Dakota Davis <davisds@student.swosu.edu>
2015-10-20 22:22:31 -0500

    cleaned up the first round

--- src/COMSCI_HW5_Dakota_Davis.java
+++ src/COMSCI_HW5_Dakota_Davis.java
@@ -8,41 +8,58 @@ public class COMSCI_HW5_Dakota_Davis {
 			int score = 0;
 	        int hardnessOfQuestion = 10;
 	        // Round 1
-	        //    Generate 2 random numbers
+	        
+	        // Generate 2 random numbers
 	        int number1 = (int)(Math.random() * hardnessOfQuestion);
 	        int number2 = (int)(Math.random() * hardnessOfQuestion);
 	        
+	        //define the correct answer
 	        int correctAnswer = number1 + number2;
 	        
-	        //  Ask the user to add these two numbers together
+	        //Prompt the user to add the two numbers together
 	        System.out.println
 	        ("What is " + number1 + " + " + number2 + "?");
 	        
-	        //  Read in their response
+	        //Read in their response
 	        Scanner input = new Scanner(System.in);
 	        int studentAnswer = input.nextInt();
-	        //  Check if the answer was correct
-	        //        IF correct
+	        
+	        //Check if the answer was correct
+	        //       IF correct
 	        if (studentAnswer == correctAnswer){
+	        	
 	            //            Tell them it was correct
-	            
+	        	System.out.println("Answer was correct");
+	        	
 	            //            Give them points
 	            score += hardnessOfQuestion;
+	            
+	            //			  Display the points
 	            System.out.println("Score:" + score);
+	            
 	            //            Make the next question harder
-	            System.out.println("Answer was correct");
+	            
 	            hardnessOfQuestion *= 10;
 	        }
 	        //        IF not correct
 	        else{
+	        	
 	            //            Tell them it was wrong
 	            System.out.println("Answer was not correct");
+	            
 	            //             Tell them the correct answer
 	            System.out.println("Correct answer was: " + correctAnswer);
+	            
 	            //            Do not give them points
+	            
 	            //            Make the next question easier
 	            if(hardnessOfQuestion>10){
 	                hardnessOfQuestion /=10;
+	                
+	       //Round 2
+	                
+	                //Repeat the process, without ints
+	                
 	            }
 	            
 	        }
